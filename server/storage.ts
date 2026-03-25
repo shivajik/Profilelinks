2168791a04d8bb8a96bf9aec901906b8bb963cb1
mayankkamriya <kamriyamayank45@gmail.com>
2026-03-25 15:57:45 +0530

    now save to wallet ui is improved

--- server/storage.ts
+++ server/storage.ts
@@ -326,7 +326,7 @@ export interface IStorage {
   getUserByUsername(username: string): Promise<User | undefined>;
   getUserByEmail(email: string): Promise<User | undefined>;
   createUser(user: InsertUser): Promise<User>;
-  updateUser(id: string, data: Partial<Pick<User, "displayName" | "bio" | "profileImage" | "coverImage" | "username" | "onboardingCompleted" | "template" | "accountType" | "teamId" | "menuTemplate" | "menuDisplayName" | "menuProfileImage" | "menuAccentColor" | "menuDescription" | "menuPhone" | "menuEmail" | "menuAddress" | "menuGoogleMapsUrl" | "menuWhatsapp" | "menuWebsite" | "mustChangePassword" | "emailVerified" | "whiteLabelEnabled" | "apiKey" | "isLtd" | "useOriginalSocialColors" | "businessPhone" | "contactFormEnabled" | "contactFormEmail" | "meetingLink">>): Promise<User | undefined>;
+  updateUser(id: string, data: Partial<Pick<User, "displayName" | "bio" | "profileImage" | "coverImage" | "username" | "onboardingCompleted" | "template" | "accountType" | "teamId" | "menuTemplate" | "menuDisplayName" | "menuProfileImage" | "menuAccentColor" | "menuDescription" | "menuPhone" | "menuEmail" | "menuAddress" | "menuGoogleMapsUrl" | "menuWhatsapp" | "menuWebsite" | "mustChangePassword" | "emailVerified" | "whiteLabelEnabled" | "apiKey" | "isLtd" | "useOriginalSocialColors" | "businessPhone" | "contactFormEnabled" | "contactFormEmail" | "meetingLink" | "showServicesOnProfile" | "showProductsOnProfile" | "showCompanyOnServices" | "showCompanyOnProducts" | "showMenuOnProfile">>): Promise<User | undefined>;
 
   getPagesByUserId(userId: string): Promise<Page[]>;
   getPageById(id: string): Promise<Page | undefined>;
@@ -468,7 +468,7 @@ export class DatabaseStorage implements IStorage {
     return user;
   }
 
-  async updateUser(id: string, data: Partial<Pick<User, "displayName" | "bio" | "profileImage" | "coverImage" | "username" | "onboardingCompleted" | "template" | "accountType" | "teamId" | "menuTemplate" | "menuDisplayName" | "menuProfileImage" | "menuAccentColor" | "menuDescription" | "menuPhone" | "menuEmail" | "menuAddress" | "menuGoogleMapsUrl" | "menuWhatsapp" | "menuWebsite" | "mustChangePassword" | "emailVerified" | "whiteLabelEnabled" | "apiKey" | "isLtd" | "useOriginalSocialColors" | "businessPhone" | "contactFormEnabled" | "contactFormEmail" | "meetingLink">>): Promise<User | undefined> {
+  async updateUser(id: string, data: Partial<Pick<User, "displayName" | "bio" | "profileImage" | "coverImage" | "username" | "onboardingCompleted" | "template" | "accountType" | "teamId" | "menuTemplate" | "menuDisplayName" | "menuProfileImage" | "menuAccentColor" | "menuDescription" | "menuPhone" | "menuEmail" | "menuAddress" | "menuGoogleMapsUrl" | "menuWhatsapp" | "menuWebsite" | "mustChangePassword" | "emailVerified" | "whiteLabelEnabled" | "apiKey" | "isLtd" | "useOriginalSocialColors" | "businessPhone" | "contactFormEnabled" | "contactFormEmail" | "meetingLink" | "showServicesOnProfile" | "showProductsOnProfile" | "showCompanyOnServices" | "showCompanyOnProducts" | "showMenuOnProfile">>): Promise<User | undefined> {
     const [user] = await db.update(users).set(data).where(eq(users.id, id)).returning();
     return user;
   }
