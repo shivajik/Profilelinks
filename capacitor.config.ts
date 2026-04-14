2a726cbc766c2bf86c756838f9a81ac346af17a9
mayankkamriya <kamriyamayank45@gmail.com>
2026-04-14 17:03:02 +0530

    we have try to setup app dev via android developement app opennign in mobile ui visible not not able to login/signup

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.visicardly.app',
+  appName: 'VisiCardly',
+  webDir: 'dist/public',
+  // server: {
+  //   url:"https://visicardly.com?forceHideBadge=true",
+  //   cleartext: true
+  // }
+};
+
+export default config;
